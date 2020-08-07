80f1801b8d586106a431d8c2673aa73eeae4bfc8
Q1zin <q1zin@mail.ru>
2020-08-07 18:52:33 +0700

    fix: modal form

--- js/main.js
+++ js/main.js
@@ -117,7 +117,7 @@ $(document).ready(function () {
     );
   }
 
-  setTimeout(googleMap, 1000);
+  setTimeout(googleMap, 2000);
 
   var map, head, insertBefore, appendChild;
   head = document.getElementsByTagName("head")[0];
